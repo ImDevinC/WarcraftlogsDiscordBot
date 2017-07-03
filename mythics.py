fe38fe9b066314439dc1d97d89e126315b89c722
Devin Collins <me@imdevinc.com>
2017-07-03 11:30:35 -0700

    Added support for future affixes

--- mythics.py
+++ mythics.py
@@ -2,6 +2,22 @@ import aiohttp
 
 BASE_URL = 'https://raider.io/api/v1/'
 
+# https://mythicpl.us/#sched
+WEEKLY_AFFIXES = [
+    ['Raging', 'Volcanic', 'Tyrannical'],
+    ['Teeming', 'Explosive', 'Fortified'],
+    ['Bolstering', 'Grievous', 'Tyrannical'],
+    ['Sanguine', 'Volvanic', 'Fortified'],
+    ['Bursting', 'Skittish', 'Tyrannical'],
+    ['Teeming', 'Quaking', 'Fortified'],
+    ['Raging', 'Necrotic', 'Tyrannical'],
+    ['Bolstering', 'Skittish', 'Fortified'],
+    ['Teeming', 'Nectroic', 'Tyrannical'],
+    ['Sanguine', 'Grevious', 'Fortified'],
+    ['Bolstering', 'Explosive', 'Tyrannical'],
+    ['Bursting', 'Quaking', 'Fortified']
+]
+
 async def getJsonData(url, params):
     async with aiohttp.ClientSession() as cs:
         async with cs.get(url, params=params) as r:
@@ -10,10 +26,22 @@ async def getJsonData(url, params):
 
 async def getAffixes():
     json_affixes = await getJsonData(BASE_URL + 'mythic-plus/affixes', {'region': 'US'})
-    if not json_affixes is None and 'title' in json_affixes:
-        return json_affixes['title']
-    else:
+    if json_affixes is None:
         return None
+    if not 'title' in json_affixes or not 'affix_details' in json_affixes:
+        return None
+    affixes = {'this_week': json_affixes['title'], 'next_week': None, 'two_weeks': None}
+    this_week = affixes['this_week'].split(', ')
+    index = WEEKLY_AFFIXES.index(this_week)
+    index += 1
+    if index == len(WEEKLY_AFFIXES):
+        index = 0
+    affixes['next_week'] = ', '.join(WEEKLY_AFFIXES[index])
+    index += 1
+    if index == len(WEEKLY_AFFIXES):
+        index = 0
+    affixes['two_weeks'] = ', '.join(WEEKLY_AFFIXES[index])
+    return affixes
 
 async def getHighest(player, realm):
     json_result = await getJsonData(BASE_URL + 'characters/profile', {'region': 'US', 'realm': realm, 'name': player, 'fields': 'mythic_plus_weekly_highest_level_runs'})
